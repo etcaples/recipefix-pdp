e59d0c6b3258346b273fa4298463529401f8e6a2
Emily Caples <etcaples@gmail.com>
2018-12-18 19:40:05 -0800

    added React Router DOM

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter } from 'react-router-dom';
+// set up router to ingest the right files
 import App from './App';
+import exampleData from '../exampleData';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App recipeData={exampleData} />
+  </BrowserRouter>,
+  document.getElementById('app'));
