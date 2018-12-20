d4720315dbc0852f1ffb79c0db41b78f15bdb1b2
Emily Caples <etcaples@gmail.com>
2018-12-19 17:43:26 -0800

    added a tag for the family name in the url pathname

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const url = require('url');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use('/shortcrust-pastry', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse the url to find which path to send recipe family data
+app.use('/:family', express.static(path.join(__dirname, 'public')));
+
+// if the url doesn't match the provided endpoint, serve a page saying the searched-for page doesn't exist
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
